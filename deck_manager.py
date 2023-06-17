f9127bb211d78a4f6714f00516e9a773a24b8272
xreymason <dupontmason@gmail.com>
2023-06-17 08:36:48 -0400

    Tool Update(deck_manager): Converted to self contianed object for multi-deck use

--- deck_manager.py
+++ deck_manager.py
@@ -1,70 +1,75 @@
 import random
 
-defalut_deck = list(range(60))
 
-current_deck = defalut_deck.copy()
+class DeckManager:
+    def __init__(self) -> None:
+        self.defalut_deck = []
+        self.current_deck = []
 
-def reset_deck():
-    """Reverts the deck back to it's original state
-    """
-    global current_deck
-    current_deck = defalut_deck.copy()
+    def reset_deck(self):
+        """Reverts the deck back to it's original state
+        """
+        self.current_deck = self.defalut_deck.copy()
 
-def shuffle_deck():
-    """Randomized the order of the deck
-    """
-    random.shuffle(current_deck)
+    def shuffle_deck(self):
+        """Randomized the order of the deck
+        """
+        random.shuffle(self.current_deck)
 
-def draw_cards(number_cards: int) -> tuple:
-    """Draws a number of cards from the top of the deck,
-    while removing them from the deck.
+    def draw_cards(self, number_cards: int) -> tuple:
+        """Draws a number of cards from the top of the deck,
+        while removing them from the deck.
 
-    :param number_cards: Number of cards to get from the deck
-    :type number_cards: int
-    :return: Cards from the deck
-    :rtype: tuple
-    """
-    drawn_cards = []
-    for _ in range(number_cards):
-        if current_deck:
-            drawn_cards.append(current_deck.pop(0))
-    return tuple(drawn_cards)
+        :param number_cards: Number of cards to get from the deck
+        :type number_cards: int
+        :return: Cards from the deck
+        :rtype: tuple
+        """
+        drawn_cards = []
+        for _ in range(number_cards):
+            if self.current_deck:
+                drawn_cards.append(self.current_deck.pop(0))
+        return tuple(drawn_cards)
 
-def draw_specific_card(card: str):
-    """Takes a specified card out of the deck if it's in the deck
+    def draw_specific_card(self, card: str):
+        """Takes a specified card out of the deck if it's in the deck
 
-    :param card: Name of the card to get
-    :type card: str
-    :return: A card from the deck
-    """
-    if card in current_deck:
-        card_index = current_deck.index(card)
-        return current_deck.pop(card_index)
+        :param card: Name of the card to get
+        :type card: str
+        :return: A card from the deck
+        """
+        if card in self.current_deck:
+            card_index = self.current_deck.index(card)
+            return self.current_deck.pop(card_index)
+        
+    def look_at_cards(self, number_cards: int, bottom: bool=False) -> tuple:
+        """Gets a list of cards from top to bottom or bottom to top of the deck,
+        without removing them from the deck.
+
+        :param number_cards: Number of cards to look at
+        :type number_cards: int
+        :param bottom: True=Look at from bottom to top, False=top to bottom
+        :type bottom: bool
+        :return: A number of cards in order from the designated side of the deck
+        :rtype: tuple
+        """
+        direction = -1 if bottom else 1
+        seen_cards = self.current_deck[::direction][:number_cards]
+        return tuple(seen_cards)
     
-def look_at_cards(number_cards: int, bottom: bool=False) -> tuple:
-    """Gets a list of cards from top to bottom or bottom to top of the deck,
-    without removing them from the deck.
+if __name__ == "__main__":
+    dm = DeckManager()
+    dm.defalut_deck = list(range(60))
+    dm.reset_deck()
+    print(dm.current_deck)
+    dm.shuffle_deck()
+    print(dm.current_deck)
+    print(dm.draw_cards(7))
+    print(dm.current_deck)
+    choice = random.choice(dm.current_deck)
+    print(choice)
+    print(dm.draw_specific_card(choice))
+    print(dm.current_deck)
 
-    :param number_cards: Number of cards to look at
-    :type number_cards: int
-    :param bottom: True=Look at from bottom to top, False=top to bottom
-    :type bottom: bool
-    :return: A number of cards in order from the designated side of the deck
-    :rtype: tuple
-    """
-    direction = -1 if bottom else 1
-    seen_cards = current_deck[::direction][:number_cards]
-    return tuple(seen_cards)
-    
-print(current_deck)
-shuffle_deck()
-print(current_deck)
-print(draw_cards(7))
-print(current_deck)
-choice = random.choice(current_deck)
-print(choice)
-print(draw_specific_card(choice))
-print(current_deck)
-
-reset_deck()
-print(current_deck)
+    dm.reset_deck()
+    print(dm.current_deck)
