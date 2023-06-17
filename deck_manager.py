12eff2328fdda4233f12247135b9f73e2887938c
xreymason <dupontmason@gmail.com>
2023-06-17 08:50:20 -0400

    Documentation Update: Added Header to deck_manager

--- deck_manager.py
+++ deck_manager.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+""" Deck Manager for any type of Card objects
+Creates abstracted functionality to modify a deck of cards in standard ways.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Mason Dupont"
+__credits__ = ["Mason Dupont"]
+__version__ = "2.0"
+__maintainer__ = "Mason Dupont"
+__license__ = "GPLv3"
+__status__ = "Development"
+
 import random
 
 
